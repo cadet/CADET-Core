6624843dbe0ab8f73a5a2a071d3f33a05a35647a
Samuel Leweke <s.leweke@fz-juelich.de>
2018-02-01 09:30:33 +0100

    Add support for CADET log output in unit tests
    By issuing a #define CADET's logging output can be enabled in unit
    tests. This may be useful for debugging test cases.

--- test/testRunner.cpp
+++ test/testRunner.cpp
@@ -10,5 +10,40 @@
 //  is available at http://www.gnu.org/licenses/gpl.html
 // =============================================================================
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+
+// Uncomment the next line to enable logging output of CADET in unit tests
+//#define CADETTEST_ENABLE_LOG
+
+
+#ifdef CADETTEST_ENABLE_LOG
+	#include "cadet/Logging.hpp"
+	#include <iostream>
+
+	class LogReceiver : public cadet::ILogReceiver
+	{
+	public:
+		LogReceiver() { }
+
+		virtual void message(const char* file, const char* func, const unsigned int line, cadet::LogLevel lvl, const char* lvlStr, const char* message)
+		{
+			std::cout << '[' << lvlStr << ": " << func << "::" << line << "] " << message << std::flush;
+		}
+	};
+#endif
+
+int main(int argc, char* argv[])
+{
+#ifdef CADETTEST_ENABLE_LOG
+	// Set LogLevel in CADET library
+	cadet::LogLevel logLevel = cadet::LogLevel::Trace;
+	LogReceiver lr;
+	cadetSetLogReceiver(&lr);
+	cadetSetLogLevel(static_cast<typename std::underlying_type<cadet::LogLevel>::type>(logLevel));
+#endif
+
+	const int result = Catch::Session().run(argc, argv);
+	return (result < 0xff ? result : 0xff);
+}
