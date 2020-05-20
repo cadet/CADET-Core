1114ddee410041bc13d8f0f9cf45e02b5eebe297
Samuel Leweke <s.leweke@fz-juelich.de>
2020-05-20 11:37:15 +0200

    Test graph algorithms more thoroughly
    Add checks of adjacency list and topological order to tests of graph
    algorithms.

--- test/Graph.cpp
+++ test/Graph.cpp
@@ -14,36 +14,121 @@
 
 #include "graph/GraphAlgos.hpp"
 
+#include <algorithm>
+
+namespace
+{
+	bool dependsOn(const int nUnits, const std::vector<int>& connections, const int unit, const int unitDep)
+	{
+		// Checks whether unit depends on unitDep
+		for (int i = 0; i < connections.size() / 6; ++i)
+		{
+			if ((connections[i * 6] == unitDep) && (connections[i * 6 + 1] == unit))
+				return true;
+		}
+		return false;
+	}
+
+	void checkTopoOrdering(const int nUnits, const std::vector<int>& connections, const std::vector<int>& topoOrder)
+	{
+		CHECK(nUnits == topoOrder.size());
+
+		// Check if each unit is in topoOrder
+		for (int i = 0; i < nUnits; ++i)
+		{
+			CHECK(std::find(topoOrder.begin(), topoOrder.end(), i) != topoOrder.end());
+		}
+
+		// Check order
+		for (int i = topoOrder.size() - 1; i >= 0; --i)
+		{
+			const int u = topoOrder[i];
+
+			// Unit u must not depend on all units before it in topoOrder
+			for (int j = 0; j < i; ++j)
+			{
+				CHECK(!dependsOn(nUnits, connections, u, topoOrder[j]));
+			}
+		}
+	}
+
+	bool contains(const int* data, int size, int val)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			if (data[i] == val)
+				return true;
+		}
+
+		return false;
+	}
+
+	void checkAdjacencyList(const int nUnits, const std::vector<int>& connections, const cadet::util::SlicedVector<int>& adjList)
+	{
+		REQUIRE(adjList.slices() == nUnits);
+
+		for (int i = 0; i < nUnits; ++i)
+		{
+			for (int j = 0; j < nUnits; ++j)
+			{
+				const int s = adjList.sliceSize(i);
+				int const* const list = adjList[i];
+
+				// Check connection from i to j
+				if (dependsOn(nUnits, connections, j, i))
+				{
+					// i connects to j, so list for i should contain j
+					CHECK(contains(list, s, j));
+				}
+				else
+				{
+					// i does not connect to j, so list for i should not contain j
+					CHECK(!contains(list, s, j));
+				}
+			}
+		}
+	}
+}
+
 TEST_CASE("Linear graph all ports all comps no cycles", "[Graph]")
 {
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, -1, -1, -1, -1,
 		1, 2, -1, -1, -1, -1,
 		2, 3, -1, -1, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+	
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
 
 TEST_CASE("Linear graph specific ports all comps no cycles", "[Graph]")
 {
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, 0, 0, -1, -1,
 		1, 2, 0, 0, -1, -1,
 		2, 3, 0, 0, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
 
 TEST_CASE("Linear graph all ports specific comps no cycles", "[Graph]")
 {
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, -1, -1, 0, 0,
 		0, 1, -1, -1, 1, 1,
@@ -55,15 +140,19 @@ TEST_CASE("Linear graph all ports specific comps no cycles", "[Graph]")
 		2, 3, -1, -1, 1, 1,
 		2, 3, -1, -1, 2, 2
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
 
 TEST_CASE("Linear graph specific ports and comps no cycles", "[Graph]")
 {
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, 0, 0, 0, 0,
 		0, 1, 0, 0, 1, 1,
@@ -75,9 +164,12 @@ TEST_CASE("Linear graph specific ports and comps no cycles", "[Graph]")
 		2, 3, 0, 0, 1, 1,
 		2, 3, 0, 0, 2, 2
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
@@ -92,6 +184,7 @@ TEST_CASE("Two ports linear graph specific ports all comps no cycles", "[Graph]"
 	         1     1     1
 	*/
 
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, 0, 0, -1, -1,
 		0, 1, 0, 1, -1, -1,
@@ -100,9 +193,12 @@ TEST_CASE("Two ports linear graph specific ports all comps no cycles", "[Graph]"
 		2, 3, 0, 0, -1, -1,
 		2, 3, 1, 0, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
@@ -117,6 +213,7 @@ TEST_CASE("Two ports linear graph specific ports and comps no cycles", "[Graph]"
 	         1     1     1
 	*/
 
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, 0, 0, 0, 0,
 		0, 1, 0, 0, 1, 1,
@@ -137,9 +234,12 @@ TEST_CASE("Two ports linear graph specific ports and comps no cycles", "[Graph]"
 		2, 3, 1, 0, 1, 1,
 		2, 3, 1, 0, 2, 2
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
@@ -152,15 +252,19 @@ TEST_CASE("N to 1 to N graph specific ports all comps no cycles", "[Graph]")
 	    O--/     \--O
 	*/
 
+	const int nUnits = 5;
 	const std::vector<int> connections = {
 		0, 2, 0, 0, -1, -1,
 		1, 2, 0, 0, -1, -1,
 		2, 3, 0, 0, -1, -1,
 		2, 4, 0, 0, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 5, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
@@ -173,21 +277,26 @@ TEST_CASE("N to 1 to N graph all ports specific comps no cycles", "[Graph]")
 	    O--/     \--O
 	*/
 
+	const int nUnits = 5;
 	const std::vector<int> connections = {
 		0, 2, 0, 0, -1, -1,
 		1, 2, 0, 1, -1, -1,
 		2, 3, 1, 0, -1, -1,
 		2, 4, 0, 0, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 5, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
 
 TEST_CASE("Linear graph specific ports twisted comps no cycles", "[Graph]")
 {
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 1, 0, 0, 0, 0,
 		0, 1, 0, 0, 1, 1,
@@ -199,9 +308,12 @@ TEST_CASE("Linear graph specific ports twisted comps no cycles", "[Graph]")
 		2, 3, 0, 0, 1, 1,
 		2, 3, 0, 0, 2, 2
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
@@ -217,6 +329,7 @@ TEST_CASE("Two cycles graph mixed ports all comps", "[Graph]")
 	    |______________|
 	*/
 
+	const int nUnits = 7;
 	const std::vector<int> connections = {
 		0, 2,  0,  0, -1, -1,
 		1, 3,  0,  0, -1, -1,
@@ -227,10 +340,11 @@ TEST_CASE("Two cycles graph mixed ports all comps", "[Graph]")
 		5, 3,  0,  1, -1, -1,
 		6, 2,  0,  1, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 7, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
-
 	REQUIRE(cycle);
 }
 
@@ -242,6 +356,7 @@ TEST_CASE("N to 1 graph specific ports mixed comps no cycles", "[Graph]")
 	    O--/     
 	*/
 
+	const int nUnits = 4;
 	const std::vector<int> connections = {
 		0, 2, 0, 0,  0,  0,
 		0, 2, 0, 0,  1,  1,
@@ -249,10 +364,114 @@ TEST_CASE("N to 1 graph specific ports mixed comps no cycles", "[Graph]")
 		1, 2, 0, 0,  0,  3,
 		2, 3, 0, 0, -1, -1
 	};
-	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), 4, connections.size() / 6);
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
 	std::vector<int> topoOrder;
 	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
 
 	REQUIRE(!cycle);
 }
 
+TEST_CASE("Self loop graph all ports all comps with cycles", "[Graph]")
+{
+	/*
+	    O--\     
+	        --O--O   /-O-\
+	    O--/         \   /
+	                  ---
+	*/
+
+	const int nUnits = 5;
+	const std::vector<int> connections = {
+		0, 3, -1, -1, -1, -1,
+		1, 3, -1, -1, -1, -1,
+		2, 2, -1, -1, -1, -1,
+		3, 4, -1, -1, -1, -1
+	};
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
+	std::vector<int> topoOrder;
+	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	REQUIRE(cycle);
+}
+
+TEST_CASE("Two connected components graph all ports all comps no cycles", "[Graph]")
+{
+	/*
+	    O--\     
+	        --O--O     O--O--O     O
+	    O--/
+	*/
+
+	const int nUnits = 8;
+	const std::vector<int> connections = {
+		0, 2, -1, -1, -1, -1,
+		1, 2, -1, -1, -1, -1,
+		2, 3, -1, -1, -1, -1,
+
+		4, 5, -1, -1, -1, -1,
+		5, 6, -1, -1, -1, -1
+	};
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
+	std::vector<int> topoOrder;
+	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
+
+	REQUIRE(!cycle);
+}
+
+TEST_CASE("Multi connected components graph all ports all comps no cycles", "[Graph]")
+{
+	/*
+		9 Units
+		0 -> 3 -> 4 -> 1 -> 7 -> 2   (5,6,8 not connected)
+	*/
+
+	const int nUnits = 9;
+	const std::vector<int> connections = {
+		4, 1, -1, -1, -1, -1,
+		0, 3, -1, -1, -1, -1,
+		1, 7, -1, -1, -1, -1,
+		3, 4, -1, -1, -1, -1,
+		7, 2, -1, -1, -1, -1
+	};
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
+	std::vector<int> topoOrder;
+	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
+
+	REQUIRE(!cycle);
+}
+
+TEST_CASE("Multi connected components graph all ports all comps no cycles 2", "[Graph]")
+{
+	/*
+		9 Units
+		0 -> 5 -> 6 -> 1 -> 7 -> 8 -> 2   (3,4 not connected)
+	*/
+
+	const int nUnits = 9;
+	const std::vector<int> connections = {
+		1, 7, -1, -1, -1, -1,
+		0, 5, -1, -1, -1, -1,
+		5, 6, -1, -1, -1, -1,
+		6, 1, -1, -1, -1, -1,
+		8, 2, -1, -1, -1, -1,
+		7, 8, -1, -1, -1, -1
+	};
+	cadet::util::SlicedVector<int> adjList = cadet::graph::adjacencyListFromConnectionList(connections.data(), nUnits, connections.size() / 6);
+	checkAdjacencyList(nUnits, connections, adjList);
+
+	std::vector<int> topoOrder;
+	const bool cycle = cadet::graph::topologicalSort(adjList, topoOrder);
+	checkTopoOrdering(nUnits, connections, topoOrder);
+
+	REQUIRE(!cycle);
+}
