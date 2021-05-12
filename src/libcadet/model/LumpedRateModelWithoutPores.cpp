42141b7839bb4ce5651bdc3ce1ec554ab8198622
Jazib Hassan <63711255+jazib-hassan-juelich@users.noreply.github.com>
2021-05-12 06:28:19 +0200

    Default to NONE adsorption model if not provided
    Selects the NONE adsorption model by default if no model is provided by
    the user. Closes #60.

--- src/libcadet/model/LumpedRateModelWithoutPores.cpp
+++ src/libcadet/model/LumpedRateModelWithoutPores.cpp
@@ -199,7 +199,11 @@ bool LumpedRateModelWithoutPores::configureModelDiscretization(IParameterProvide
 	clearBindingModels();
 	_binding.push_back(nullptr);
 
-	_binding[0] = helper.createBindingModel(paramProvider.getString("ADSORPTION_MODEL"));
+	if (paramProvider.exists("ADSORPTION_MODEL"))
+		_binding[0] = helper.createBindingModel(paramProvider.getString("ADSORPTION_MODEL"));
+	else
+		_binding[0] = helper.createBindingModel("NONE");
+
 	if (!_binding[0])
 		throw InvalidParameterException("Unknown binding model " + paramProvider.getString("ADSORPTION_MODEL"));
 
