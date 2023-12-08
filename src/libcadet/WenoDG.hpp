55e0480855c04bbd3ac809768565ccaed917521e
Jan Breuer <j.m.br@gmx.de>
2023-12-08 13:48:52 +0100

    WIP add subcell limiting

--- src/libcadet/WenoDG.hpp
+++ src/libcadet/WenoDG.hpp
@@ -29,7 +29,6 @@
 namespace cadet
 {
 
-	//template<typename StateType> // todo
 	class WENOLimiter {
 	public:
 		virtual ~WENOLimiter() {}
@@ -60,12 +59,8 @@ namespace cadet
 				return 0.0;
 		}
 		double call(active u0, active u1, active u2) override {
-			if (std::signbit(u0.getValue()) == std::signbit(u1.getValue()) && std::signbit(u0.getValue()) == std::signbit(u2.getValue())) {
-				if (std::signbit(u0.getValue())) // i.e. negative sign
-					return -static_cast<double>(u0);
-				else
-					return static_cast<double>(u0);
-			}
+			if (std::signbit(u0.getValue()) == std::signbit(u1.getValue()) && std::signbit(u0.getValue()) == std::signbit(u2.getValue()))
+				return std::copysign(std::min(std::abs(u0.getValue()), std::min(std::abs(u1.getValue()), std::abs(u2.getValue()))), u0.getValue());
 			else
 				return 0.0;
 		}
@@ -108,20 +103,9 @@ namespace cadet
 	public:
 
 		/**
-		 * @brief Boundary treatment method determines how the reconstruction handles insufficient available elements (i.e., less elements available than stencil size)
-		 */
-		enum class BoundaryTreatment : int
-		{
-			ReduceOrder = 0, //!< Reduce the order of the WENO method such that the stencil is small enough
-			ZeroWeights = 1,  //!< Set weights of WENO method to 0 for unavailable elements
-			ZeroWeightsForPnotZero = 2, //!< Set weights of WENO method to 0 for unavailable elements, except for the first cell (order is reduced to 1)
-			LargeGhostNodes = 3,
-		};
-
-		/**
 		 * @brief Creates the WENO scheme
 		 */
-		//WenoDG() : _order(maxOrder()), _boundaryTreatment(BoundaryTreatment::ReduceOrder), _intermediateValues(3 * maxOrder() * sizeof(active)) { }
+		WenoDG() : _weno_gamma(nullptr), _w(nullptr), _wHat(nullptr), _beta(nullptr), weno_limiter(nullptr) { }
 
 		~WenoDG() CADET_NOEXCEPT
 		{
@@ -129,32 +113,85 @@ namespace cadet
 			delete[] _w;
 			delete[] _wHat;
 			delete[] _beta;
-			delete[] _troubled_cells;
 		}
 
-		void init(const double eps, const double r, const double gamma, const unsigned int nCells, const unsigned int nComp, const bool trackTroubledCells = false) {
+		void init(std::string limiter, const double eps, const double r, const double gamma, double* invWeights, Eigen::MatrixXd polyDerM, const unsigned int nCells, const unsigned int nNodes, const unsigned int nComp) {
 
-			_weno_gamma = new double[3]{ (1 - gamma) / 2.0, gamma, (1 - gamma) / 2.0 };
+			_nComp = nComp;
+			_nCells = nCells;
+			_nNodes = nNodes;
+
+			_polyDerM = polyDerM;
+			_LGLweights.resize(nNodes);
+			for (int node = 0; node < nNodes; node++)
+				_LGLweights[node] = 1.0 / invWeights[node];
+
+			_weno_gamma = new double[3]{ (1.0 - gamma) / 2.0, gamma, (1.0 - gamma) / 2.0 };
 			_weno_r = r;
 			_weno_eps = eps;
-			if (trackTroubledCells)
-				_troubled_cells = new double[nCells * nComp];
-			else
-				_troubled_cells = nullptr;
+
+			if (limiter == "MINMOD")
+				weno_limiter = std::make_unique<MinmodWENO>();
+			else if (limiter == "TVB_MINMOD")
+				weno_limiter = std::make_unique<TVBMinmodWENO>();
 		}
 
-		inline double* troubledCells() const CADET_NOEXCEPT { return &_troubled_cells[0]; }
+		void setDeltaZ(active deltaZ) { _deltaZ = deltaZ; }
+
+		template<typename StateType>
+		void calcSmoothness(const StateType* const localC, const int strideNode, const int comp, const int cell, double* troubled_cells)
+		{
+			troubled_cells[comp + cell * _nComp] = 2.0;
+
+			Eigen::Map<const Vector<StateType, Dynamic>, 0, InnerStride<Dynamic>> _C(localC, _nNodes, InnerStride<Dynamic>(strideNode));
+
+			if (cell > 0 && cell < _nCells - 1) // todo boundary treatment
+			{
+				// todo store mass matrix and LGL weights additionally to respective inverse
+				// todo overwrite values instead of recalculation // todo deltaZ ParamType
+				_pAvg0 = 1.0 / static_cast<double>(_deltaZ) * (_LGLweights.array() * _C.segment((cell - 1) * _nNodes, _nNodes).template cast<StateType>().array()).sum();
+				_pAvg1 = 1.0 / static_cast<double>(_deltaZ) * (_LGLweights.array() * _C.segment(cell * _nNodes, _nNodes).template cast<StateType>().array()).sum();
+				_pAvg2 = 1.0 / static_cast<double>(_deltaZ) * (_LGLweights.array() * _C.segment((cell + 1) * _nNodes, _nNodes).template cast<StateType>().array()).sum();
+
+				_uTilde = _C[cell * _nNodes + _nNodes - 1] - _pAvg1; // average minus inner interface value on right face
+				_u2Tilde = _pAvg1 - _C[cell * _nNodes]; // average minus inner interface value on left face
+
+				double trigger1 = weno_limiter->call(_uTilde, _pAvg2 - _pAvg1, _pAvg1 - _pAvg0);
+				double trigger2 = weno_limiter->call(_u2Tilde, _pAvg2 - _pAvg1, _pAvg1 - _pAvg0);
+
+				double M2 = (_polyDerM * _polyDerM * _C.segment(cell * _nNodes, _nNodes).template cast<double>()).maxCoeff();
+				//double u_x = (_polyDerM * _C.segment(cell * _nNodes, _nNodes)).maxCoeff();
+				double M_ = 2.0 / 3.0 * M2;
+				//double hmpf = 2.0 / 9.0 * (3.0 + 10.0 * M2) * M2 * _deltaZ / (_deltaZ + 2.0 * u_x * _deltaZ);
+
+				// reconstruct if cell is troubled, i.e. potential oscillations
+				if (abs(trigger1 - _uTilde) > 1e-8 || abs(trigger2 - _u2Tilde) > 1e-8)
+				{
+					if (abs(_uTilde) > M_ * _deltaZ * _deltaZ || abs(_u2Tilde) > M_ * _deltaZ * _deltaZ)
+						// mark troubled cell
+						troubled_cells[comp + cell * _nComp] = 1.0;
+				}
+			}
+		}
 
 		//private:
 
 		// indicator for troubled cells
 		std::unique_ptr<WENOLimiter> weno_limiter;
 
+		int _nNodes;
+		int _nComp;
+		int _nCells;
+
 		// WENO parameters
 		double* _weno_gamma;
 		double _weno_r = 2.0;
 		double _weno_eps = 1e-6;
+
 		// weights, smoothness indicator, integral values.
+		Eigen::VectorXd _LGLweights; //!< DG method LGL weights, i.e. subcell sizes
+		Eigen::MatrixXd _polyDerM; //!< DG method LGL weights, i.e. subcell sizes
+		active _deltaZ;
 		active* _wHat = new active[3]{ 0.0, 0.0, 0.0 };
 		active* _w = new active[3]{ 0.0, 0.0, 0.0 };
 		active _wSum = 0.0;
@@ -167,8 +204,6 @@ namespace cadet
 		active _avgUdeltaMinus = 0.0;
 		active _uTilde = 0.0;
 		active _u2Tilde = 0.0;
-		// mark troubled cells (smoothness indicator)
-		double* _troubled_cells;
 
 	};
 
