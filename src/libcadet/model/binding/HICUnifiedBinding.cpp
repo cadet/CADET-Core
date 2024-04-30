c6880be3f8b6a9b6e77cca7d4bcb08d9a868ed79
r.jaepel <ronald.jaepel@gmail.com>
2024-04-30 14:56:59 +0200

    Add pH dependence

--- src/libcadet/model/binding/HICUnifiedBinding.cpp
+++ src/libcadet/model/binding/HICUnifiedBinding.cpp
@@ -35,15 +35,18 @@
 			{ "type": "ScalarParameter", "varName": "beta0", "confName": "HICUNI_BETA0"},
 			{ "type": "ScalarParameter", "varName": "beta1", "confName": "HICUNI_BETA1"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "kA", "confName": "HICUNI_KA"},
+			{ "type": "ScalarComponentDependentParameter", "varName": "kALin", "confName": "HICUNI_KA_LIN"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "kD", "confName": "HICUNI_KD"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "kP", "confName": "HICUNI_KP"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "kS", "confName": "HICUNI_KS"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "epsilon", "confName": "HICUNI_EPSILON"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "nu", "confName": "HICUNI_NU"},
+			{ "type": "ScalarComponentDependentParameter", "varName": "nuLin", "confName": "HICUNI_NU_LIN"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "qMax", "confName": "HICUNI_QMAX"}
 		],
 	"constantParameters":
 		[
+			{ "type": "ScalarParameter", "varName": "refPh", "default": 7.0, "objName": "referencePh", "confName": "HICUNI_PH_REF"},
 			{ "type": "ScalarParameter", "varName": "rho", "default": 0.00003348, "objName": "osmoticEffect", "confName": "HICUNI_RHO"}
 
 		]
@@ -55,13 +58,16 @@
  beta0 = bulk-like ordered water molecules at infinitely diluted salt concentration
  beta1 = influence of salt concentration on bulk-like ordered water molecules
  kA = Adsorption constant
+ kALin = Linear dependency of ka on pH
  kD = Desorption constant
  kP = Influence of protein concentration on protein activity
  kS = Influence of salt concentration on protein activity
  epsilon = Influence of bound protein concentration on bound protein activity
  nu = Number of binding sites
+ nuLin = Linear dependency of nu on pH
  qMax = Maximum binding capacity
  rh0 = osmotic effect of salt concentration on water activity. Calculated as osmotic_coefficient * molar_weight_of_water * ion_number in
+ refpH = reference pH (defaults to 7)
 */
 
 namespace cadet
@@ -75,8 +81,10 @@ namespace cadet
 		inline bool HICUNIFIEDParamHandler::validateConfig(unsigned int nComp, unsigned int const* nBoundStates)
 		{
 			if ((_kA.size() != _kD.size()) || (_kA.size() != _nu.size()) || (_kA.size() != _qMax.size())
-				|| (_kA.size() != _kP.size()) || (_kA.size() != _kS.size()) || (_kA.size() != _epsilon.size()) || (_kA.size() < nComp))
-				throw InvalidParameterException("HICUNI_KA, HICUNI_KD, HICUNI_NU, and HICUNI_QMAX have to have the same size");
+				|| (_kA.size() != _kP.size()) || (_kA.size() != _kS.size()) || (_kA.size() != _epsilon.size())
+				|| (_kA.size() != _nuLin.size()) || (_kA.size() != _kALin.size())
+				|| (_kA.size() < nComp))
+				throw InvalidParameterException("HICUNI_KA, HICUNI_KD, HICUNI_NU, and HICUNI_QMAX, HICUNI_KA_LIN, HICUNI_NU_LIN have to have the same size");
 
 			return true;
 		}
@@ -86,8 +94,10 @@ namespace cadet
 		inline bool ExtHICUNIFIEDParamHandler::validateConfig(unsigned int nComp, unsigned int const* nBoundStates)
 		{
 			if ((_kA.size() != _kD.size()) || (_kA.size() != _nu.size()) || (_kA.size() != _qMax.size())
-				|| (_kA.size() != _kP.size()) || (_kA.size() != _kS.size()) || (_kA.size() != _epsilon.size()) || (_kA.size() < nComp))
-				throw InvalidParameterException("KA, KD, NU, and QMAX have to have the same size");
+				|| (_kA.size() != _kP.size()) || (_kA.size() != _kS.size()) || (_kA.size() != _epsilon.size())
+				|| (_kA.size() != _nuLin.size()) || (_kA.size() != _kALin.size())
+				|| (_kA.size() < nComp))
+				throw InvalidParameterException("HICUNI_KA, HICUNI_KD, HICUNI_NU, and HICUNI_QMAX, HICUNI_KA_LIN, HICUNI_NU_LIN have to have the same size");
 
 			return true;
 		}
@@ -97,7 +107,10 @@ namespace cadet
 		 * @brief Defines a unified HIC isotherm combining publications by Deichert et al 2010, Mollerup et al 2006, and Jäpel and Buyel 2022
 		 * @details Implements a unified HIC isotherm : \f[ \begin{align}
 		 *				\beta &= \beta_0 e^{c_{p,0}\beta_1}\\
-		 *				\frac{\mathrm{d}q_i}{\mathrm{d}t} &= k_{a,i} c_{p,i}e^{k_{p,i} c_{p,i}+k_{s,i} c_{s}}(1-\sum{\frac{q_p}{q_{max}}})^{ν_i} -k_{d,i} q_{p,i}(1+\epsilon q_{p,i}) (e^{ρc_{s}})^{ν_i β_0 e^{β_1 c_s}}
+		 *				k_{a,i} &= k_{a,i,0} \exp\left( k_{a,i,lin} (\mathrm{pH}-\mathrm{pH}_{ref})\right)\\
+		 *				\nu_i &= \nu_{i,0}  + \nu_{i,lin} (\mathrm{pH}-\mathrm{pH}_{ref})\\
+		 *				\frac{\mathrm{d}q_i}{\mathrm{d}t} &= k_{a,i} c_{p,i}e^{k_{p,i} c_{p,i}+k_{s,i} c_{s}}(1-\sum{\frac{q_p}{q_{max}}})^{\nu_i}
+		 *													-k_{d,i} q_{p,i}(1+\epsilon q_{p,i}) (e^{\rho c_{s}})^{\nu_i \beta_0 e^{\beta_1 c_s}}
 		 *			\end{align}  \f]
 		 *          Component @c 0 is assumed to be salt without a bound state. Multiple bound states are not supported.
 		 *          Components without bound state (i.e., salt and non-binding components) are supported.
@@ -122,6 +135,10 @@ namespace cadet
 				if (nBound[0] != 0)
 					throw InvalidParameterException("HICUNIFIED binding model requires exactly zero bound states for salt component");
 
+
+				// Guarantee that modifier component is non-binding
+				if (nBound[1] != 0)
+					throw InvalidParameterException("HICUNIFIED binding model requires non-binding pH modifier component (NBOUND[1] = 0)");
 				return res;
 			}
 
@@ -167,6 +184,10 @@ namespace cadet
 
 				typename ParamHandler_t::ParamsHandle const _p = _paramHandler.update(t, secIdx, colPos, _nComp, _nBoundStates, workSpace);
 
+				// Pseudo component 1 is pH
+				const CpStateType pH = yCp[1];
+				const ParamType refPh = static_cast<ParamType>(_p->refPh);
+
 				const ParamType beta0 = static_cast<ParamType>(_p->beta0);
 				const ParamType beta1 = static_cast<ParamType>(_p->beta1);
 
@@ -183,7 +204,7 @@ namespace cadet
 				StateParamType qSum = 1.0;
 
 				unsigned int bndIdx = 0;
-				for (int i = 0; i < _nComp; ++i)
+				for (int i = 2; i < _nComp; ++i)
 				{
 					// Skip components without bound states (bound state index bndIdx is not advanced)
 					if (_nBoundStates[i] == 0)
@@ -196,7 +217,7 @@ namespace cadet
 				}
 
 				bndIdx = 0;
-				for (int i = 0; i < _nComp; ++i)
+				for (int i = 2; i < _nComp; ++i)
 				{
 					// Skip components without bound states (bound state index bndIdx is not advanced)
 					if (_nBoundStates[i] == 0)
@@ -204,15 +225,18 @@ namespace cadet
 
 					const ParamType kD = static_cast<ParamType>(_p->kD[i]);
 					const ParamType kA = static_cast<ParamType>(_p->kA[i]);
+					const ParamType kALin = static_cast<ParamType>(_p->kALin[i]);
 					const ParamType kP = static_cast<ParamType>(_p->kP[i]);
 					const ParamType kS = static_cast<ParamType>(_p->kS[i]);
 					const ParamType nu = static_cast<ParamType>(_p->nu[i]);
+					const ParamType nuLin = static_cast<ParamType>(_p->nuLin[i]);
 					const ParamType epsilon = static_cast<ParamType>(_p->epsilon[i]);
 
-					/*res[bndIdx] = kD * y[bndIdx] * pow(water_activity, nu * beta)
-						- kA * pow(qSum, nu) * yCp[i];*/
-					res[bndIdx] = kD * y[bndIdx] * (1 + epsilon * y[bndIdx]) * pow(water_activity, nu * beta)
-						- kA * pow(qSum, nu) * yCp[i] * exp(kP * yCp[i] + kS * yCp[0]);
+					const CpStateParamType kApH = kA * exp(kALin * (pH - refPh));
+					const CpStateParamType nupH = nu + nuLin * (pH - refPh);
+
+					res[bndIdx] = kD * y[bndIdx] * (1 + epsilon * y[bndIdx]) * pow(water_activity, nupH * beta)
+						- kApH * pow(qSum, nupH) * yCp[i] * exp(kP * yCp[i] + kS * yCp[0]);
 
 					// Next bound component
 					++bndIdx;
@@ -228,6 +252,10 @@ namespace cadet
 
 				using std::pow, std::exp;
 
+				// Pseudo component 1 is pH
+				const double pH = yCp[1];
+				const double refPh = static_cast<double>(_p->refPh);
+
 				const double beta0 = static_cast<double>(_p->beta0);
 				const double beta1 = static_cast<double>(_p->beta1);
 
@@ -239,7 +267,7 @@ namespace cadet
 				double qSum = 1.0;
 
 				unsigned int bndIdx = 0;
-				for (int i = 0; i < _nComp; ++i)
+				for (int i = 2; i < _nComp; ++i)
 				{
 					// Skip components without bound states (bound state index bndIdx is not advanced)
 					if (_nBoundStates[i] == 0)
@@ -252,7 +280,7 @@ namespace cadet
 				}
 
 				bndIdx = 0;
-				for (int i = 0; i < _nComp; ++i)
+				for (int i = 2; i < _nComp; ++i)
 				{
 					// Skip components without bound states (bound state index bndIdx is not advanced)
 					if (_nBoundStates[i] == 0)
@@ -264,15 +292,21 @@ namespace cadet
 					const double kS = static_cast<double>(_p->kS[i]);
 					const double nu = static_cast<double>(_p->nu[i]);
 					const double epsilon = static_cast<double>(_p->epsilon[i]);
+					const double kALin = static_cast<double>(_p->kALin[i]);
+					const double nuLin = static_cast<double>(_p->nuLin[i]);
+
+					const double kApH = kA * exp(kALin * (pH - refPh));
+					const double nupH = nu + nuLin * (pH - refPh);
+
 
 					// dres_i / dc_{p,0}
-					jac[-bndIdx - offsetCp] = kD * y[bndIdx] * (1 + epsilon * y[bndIdx]) * (-rho) * nu * beta0 * (beta1 * yCp[0] + 1) *
-						exp(beta1 * yCp[0] - rho * nu * beta0 * exp(beta1 * yCp[0]) * yCp[0])
-						- kA * yCp[i] * pow(qSum, nu) * exp(kP * yCp[i] + kS * yCp[0]) * kS;
+					jac[-bndIdx - offsetCp] = kD * y[bndIdx] * (1 + epsilon * y[bndIdx]) * (-rho) * nupH * beta0 * (beta1 * yCp[0] + 1) *
+						exp(beta1 * yCp[0] - rho * nupH * beta0 * exp(beta1 * yCp[0]) * yCp[0])
+						- kApH * yCp[i] * pow(qSum, nupH) * exp(kP * yCp[i] + kS * yCp[0]) * kS;
 
 					// dres_i / dc_{p,i}
-					jac[i - bndIdx - offsetCp] = -kA * pow(qSum, nu) * exp(kP * yCp[i] + kS * yCp[0])
-						- kA * yCp[i] * pow(qSum, nu) * exp(kP * yCp[i] + kS * yCp[0]) * kP;
+					jac[i - bndIdx - offsetCp] = -kApH * pow(qSum, nupH) * exp(kP * yCp[i] + kS * yCp[0])
+						- kApH * yCp[i] * pow(qSum, nupH) * exp(kP * yCp[i] + kS * yCp[0]) * kP;
 					// Getting to c_{p,i}: -bndIdx takes us to q_0, another -offsetCp to c_{p,0} and a +i to c_{p,i}.
 					//                     This means jac[i - bndIdx - offsetCp] corresponds to c_{p,i}.
 
@@ -286,14 +320,14 @@ namespace cadet
 							continue;
 
 						// dres_i / dq_j
-						jac[bndIdx2 - bndIdx] = kA * yCp[i] * nu * pow(qSum, nu - 1) / (static_cast<double>(_p->qMax[j])) * exp(kP * yCp[i] + kS * yCp[0]);
+						jac[bndIdx2 - bndIdx] = kApH * yCp[i] * nupH * pow(qSum, nupH - 1) / (static_cast<double>(_p->qMax[j])) * exp(kP * yCp[i] + kS * yCp[0]);
 						// Getting to q_j: -bndIdx takes us to q_0, another +bndIdx2 to q_j. This means jac[bndIdx2 - bndIdx] corresponds to q_j.
 
 						++bndIdx2;
 					}
 
 					// Add to dres_i / dq_i
-					jac[0] += (1 + 2 * epsilon * y[bndIdx]) * kD * pow(water_activity, nu * beta);
+					jac[0] += (1 + 2 * epsilon * y[bndIdx]) * kD * pow(water_activity, nupH * beta);
 
 					// Advance to next flux and Jacobian row
 					++bndIdx;
