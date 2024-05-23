161fc4232d8c62bb56a5e4128156ed3ee7db9b08
Jan Breuer <j.m.br@gmx.de>
2024-05-23 15:26:08 +0200

    Increase timing precision to nanoseconds when threading disabled

--- include/common/Timer.hpp
+++ include/common/Timer.hpp
@@ -337,7 +337,7 @@ namespace cadet
 
 				inline double stopCore() const
 				{
-					return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - _startTime).count();
+					return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - _startTime).count() * 1e-9;
 				}
 
 			protected:
