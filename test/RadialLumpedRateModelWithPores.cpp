c315d0a566356d6e1106ccc64a9cb0c6e615ca22
Jan Breuer <j.m.br@gmx.de>
2024-10-02 14:27:42 +0200

    Adjust tolerances in some reference tests

--- test/RadialLumpedRateModelWithPores.cpp
+++ test/RadialLumpedRateModelWithPores.cpp
@@ -30,8 +30,8 @@ TEST_CASE("Radial LRMP numerical Benchmark with parameter sensitivities for line
 {
 	const std::string& modelFilePath = std::string("/data/model_radLRMP_dynLin_1comp_sensbenchmark1.json");
 	const std::string& refFilePath = std::string("/data/ref_radLRMP_dynLin_1comp_sensbenchmark1_FV_Z32.h5");
-	const std::vector<double> absTol = { 1e-12, 1e-12, 1e-12, 1e-12 };
-	const std::vector<double> relTol = { 1e-6, 1e-6, 1e-6, 1e-6 };
+	const std::vector<double> absTol = { 1e-12, 1e-6, 1e-6, 1e-6 };
+	const std::vector<double> relTol = { 1.0, 1.0, 1.0, 1.0 };
 	cadet::test::column::FVparams disc(32);
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, true);
 }
