1b4ae4f44f4e8f42f0eabcc4f3413805b996f0e6
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2024-12-07 08:27:24 +0100

    Set `isParticleLumped` to true in LRMP

--- src/libcadet/model/LumpedRateModelWithPoresDG2D.hpp
+++ src/libcadet/model/LumpedRateModelWithPoresDG2D.hpp
@@ -365,7 +365,7 @@ protected:
 		virtual bool hasParticleMobilePhase() const CADET_NOEXCEPT { return true; }
 		virtual bool hasSolidPhase() const CADET_NOEXCEPT { return _disc.strideBound[_disc.nParType] > 0; }
 		virtual bool hasVolume() const CADET_NOEXCEPT { return false; }
-		virtual bool isParticleLumped() const CADET_NOEXCEPT { return false; }
+		virtual bool isParticleLumped() const CADET_NOEXCEPT { return true; }
 		virtual bool hasPrimaryExtent() const CADET_NOEXCEPT { return true; }
 		virtual bool hasSmoothnessIndicator() const CADET_NOEXCEPT { return false; }
 
