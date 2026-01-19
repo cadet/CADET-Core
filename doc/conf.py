f025acadabaaac3f702910ab9aacae5929d91d37
Jan Breuer <j.m.br@gmx.de>
2026-01-19 19:04:55 +0100

    Docs: remove chore/dev_guide branch from website

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ smv_tag_whitelist = r"^v\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$" # Releases and P
 smv_released_pattern = smv_released_pattern = (
     r"^(heads|remotes/origin)/\d+\.\d+\.X$"
     r"|^tags/v\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$") # Releases and Pre-Releases
-smv_branch_whitelist = r"^(master|chore/developer_guide|v5\.1\.X)$"
+smv_branch_whitelist = r"^(master|v5\.1\.X)$"
 smv_remote_whitelist = r"^origin$"          # Use branches from remote origin
 smv_outputdir_format = '{ref.name}'         # Use the branch/tag name
 
