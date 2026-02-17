62c5295c556edbe70bda1afb47c059ea2c36082d
Flynn Zhang <zwendi823@gmail.com>
2026-02-16 20:17:07 -0500

    Update competitive adsorption calculations, add softplus and internal pSalt calculations

--- src/libcadet/model/binding/AffinityComplexTitrationBinding.cpp
+++ src/libcadet/model/binding/AffinityComplexTitrationBinding.cpp
@@ -17,11 +17,85 @@
 #include "model/Parameters.hpp"
 #include "LocalVector.hpp"
 #include "SimulationTypes.hpp"
+#include "MathUtil.hpp"
 
+#include <cmath>
 #include <functional>
 #include <unordered_map>
 #include <string>
 #include <vector>
+
+namespace
+{
+	constexpr double ln10 = 2.30258509299404568402;
+	constexpr double minSaltConcentration = 1e-30;
+	constexpr double softplusTransitionFactor = 20.0;
+	constexpr double minApparentCapacity = 1e-12;
+
+	template <typename T>
+	inline T stableActGate(T const& eta, T const& pKa, T const& saltAxis)
+	{
+		using std::pow;
+		return 1.0 / (1.0 + pow(10.0, eta * (pKa - saltAxis)));
+	}
+
+	template <>
+	inline double stableActGate<double>(double const& eta, double const& pKa, double const& saltAxis)
+	{
+		const double x = ln10 * eta * (pKa - saltAxis);
+		if (x > 0.0)
+		{
+			const double e = std::exp(-x);
+			return e / (1.0 + e);
+		}
+
+		const double e = std::exp(x);
+		return 1.0 / (1.0 + e);
+	}
+
+	inline double stableActGateDerivSalt(double const eta, double const gate)
+	{
+		return ln10 * eta * gate * (1.0 - gate);
+	}
+
+	template <typename T>
+	inline T softplusScaled(T const& x, T const& beta)
+	{
+		using std::exp;
+		using std::log;
+		return log(1.0 + exp(beta * x)) / beta;
+	}
+
+	template <>
+	inline double softplusScaled<double>(double const& x, double const& beta)
+	{
+		const double bx = beta * x;
+		if (bx > 0.0)
+			return (bx + std::log1p(std::exp(-bx))) / beta;
+
+		return std::log1p(std::exp(bx)) / beta;
+	}
+
+	template <typename T>
+	inline T sigmoid(T const& x)
+	{
+		using std::exp;
+		return 1.0 / (1.0 + exp(-x));
+	}
+
+	template <>
+	inline double sigmoid<double>(double const& x)
+	{
+		if (x >= 0.0)
+		{
+			const double e = std::exp(-x);
+			return 1.0 / (1.0 + e);
+		}
+
+		const double e = std::exp(x);
+		return e / (1.0 + e);
+	}
+}
 
 /*<codegen>
 {
@@ -36,6 +110,10 @@
 			{ "type": "ScalarComponentDependentParameter", "varName": "etaG", "confName": "ACT_ETAG"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "pKaA", "confName": "ACT_PKAA"},
 			{ "type": "ScalarComponentDependentParameter", "varName": "pKaG", "confName": "ACT_PKAG"}
+		],
+	"constantParameters":
+		[
+			{ "type": "ScalarBoolParameter", "varName": "usePSalt", "confName": "ACT_USE_P_SALT"}
 		]
 }
 </codegen>*/
@@ -98,7 +176,7 @@ namespace cadet
 
 				// Guarantee that pH is not a bound speies
 				if (nBound[0] != 0)
-					throw InvalidParameterException("Affinity complex titration binding model requires the first component pH to be non-binding");
+					throw InvalidParameterException("Affinity complex titration binding model requires the first component (pSalt or salt concentration) to be non-binding");
 
 				for (int i = 0; i < nComp; ++i)
 				{
@@ -126,9 +204,15 @@ namespace cadet
 			{
 				typename ParamHandler_t::ParamsHandle const p = _paramHandler.update(t, secIdx, colPos, _nComp, _nBoundStates, workSpace);
 
+				using CpStateParamType = typename DoubleActivePromoter<CpStateType, ParamType>::type;
+				using ResParamType = typename DoubleActivePromoter<ResidualType, ParamType>::type;
+				using std::log;
+
 				// Protein fluxes
 				ResidualType qSum = 0.0;
 				unsigned int bndIdx = 0;
+				const CpStateParamType saltAxis = _paramHandler.usePSalt().get() ? yCp[0] : -log(yCp[0] + static_cast<ParamType>(minSaltConcentration)) / static_cast<ParamType>(ln10);
+				const ParamType saltAxisParam = static_cast<ParamType>(saltAxis);
 				for (int i = 0; i < _nComp; ++i)
 				{
 					// Skip components without bound states (bound state index bndIdx is not advanced)
@@ -136,7 +220,7 @@ namespace cadet
 						continue;
 
 					// y is only defined for components that have a bound state
-					qSum -= y[bndIdx] / static_cast<ParamType>(p->qMax[i]);
+					qSum += y[bndIdx];
 
 					// Next bound component
 					++bndIdx;
@@ -151,9 +235,18 @@ namespace cadet
 						continue;
 
 					// Residual
-					const ResidualType qSum_local = qSum + 1.0 / (1.0 + pow(10.0, static_cast<ParamType>(p->etaA[i]) * (static_cast<ParamType>(p->pKaA[i]) - yCp[0])));
-					const ResidualType kA_local = static_cast<ParamType>(p->kA[i]) / (1.0 + pow(10.0, static_cast<ParamType>(p->etaG[i]) * (static_cast<ParamType>(p->pKaG[i]) - yCp[0])));
-					res[bndIdx] = static_cast<ParamType>(p->kD[i]) * y[bndIdx] - kA_local * yCp[i] * static_cast<ParamType>(p->qMax[i]) * qSum_local;
+					const ParamType pKaAaxis = static_cast<ParamType>(p->pKaA[i]);
+					const ParamType pKaGaxis = static_cast<ParamType>(p->pKaG[i]);
+					const ResParamType f_A = stableActGate(static_cast<ParamType>(p->etaA[i]), pKaAaxis, saltAxisParam);
+					const ResParamType f_G = stableActGate(static_cast<ParamType>(p->etaG[i]), pKaGaxis, saltAxisParam);
+
+					const ResParamType qApp = static_cast<ParamType>(p->qMax[i]) * f_A;
+					const ResParamType qFree_local = qApp - qSum;
+					const ResParamType qScale = (qApp > static_cast<ParamType>(minApparentCapacity)) ? qApp : static_cast<ParamType>(minApparentCapacity);
+					const ResParamType beta = static_cast<ParamType>(softplusTransitionFactor) / qScale;
+					const ResParamType qFree_eff = softplusScaled(qFree_local, beta);
+					const ResParamType kA_local = static_cast<ParamType>(p->kA[i]) * f_G;
+					res[bndIdx] = static_cast<ResidualType>(static_cast<ParamType>(p->kD[i]) * y[bndIdx] - kA_local * yCp[i] * qFree_eff);
 
 					// Next bound component
 					++bndIdx;
@@ -170,13 +263,15 @@ namespace cadet
 				// Protein flux
 				double qsum = 0.0;
 				int bndIdx = 0;
+				const double saltAxis = _paramHandler.usePSalt().get() ? yCp[0] : -std::log(yCp[0] + minSaltConcentration) / ln10;
+				const double dSaltAxis_dC0 = _paramHandler.usePSalt().get() ? 1.0 : -1.0 / ((yCp[0] + minSaltConcentration) * ln10);
 				for (int i = 0; i < _nComp; ++i)
 				{
 					// Skip components without bound states
 					if (_nBoundStates[i] == 0)
 						continue;
 
-					qsum -= y[bndIdx] / static_cast<double>(p->qMax[i]);
+					qsum += y[bndIdx];
 
 					// Next bound component
 					++bndIdx;
@@ -190,25 +285,31 @@ namespace cadet
 						continue;
 
 					// local variables to aid the calculation of the jacobian
-					const double qMax_denominator = 1.0 + pow(10.0, static_cast<double>(p->etaA[i]) * (static_cast<double>(p->pKaA[i]) - yCp[0]));
-					const double f_A = 1.0 / qMax_denominator;
+					const double pKaAaxis = static_cast<double>(p->pKaA[i]);
+					const double pKaGaxis = static_cast<double>(p->pKaG[i]);
+					const double f_A = stableActGate(static_cast<double>(p->etaA[i]), pKaAaxis, saltAxis);
+					const double f_G = stableActGate(static_cast<double>(p->etaG[i]), pKaGaxis, saltAxis);
 
-					const double ka_denominator = 1.0 + pow(10.0, static_cast<double>(p->etaG[i]) * (static_cast<double>(p->pKaG[i]) - yCp[0]));
-					const double f_G = 1.0 / ka_denominator;
+					const double f_A_deriv = stableActGateDerivSalt(static_cast<double>(p->etaA[i]), f_A);
+					const double f_G_deriv = stableActGateDerivSalt(static_cast<double>(p->etaG[i]), f_G);
 
-					const double qSum_local = qsum + f_A;
+					const double qApp = static_cast<double>(p->qMax[i]) * f_A;
+					const double qFree_local = qApp - qsum;
+					const double qScale = (qApp > minApparentCapacity) ? qApp : minApparentCapacity;
+					const double beta = softplusTransitionFactor / qScale;
+					const double qFree_eff = softplusScaled(qFree_local, beta);
+					const double dqFreeEff_dqFree = sigmoid(beta * qFree_local);
+
 					const double qmax_times_ka = static_cast<double>(p->kA[i]) * static_cast<double>(p->qMax[i]);
-
-					const double f_A_deriv = std::log(10.0) * static_cast<double>(p->etaA[i]) * f_A * f_A * (qMax_denominator - 1.0);
-					const double f_G_deriv = std::log(10.0) * static_cast<double>(p->etaG[i]) * f_G * f_G * (ka_denominator - 1.0);
+					const double kA_times_fG = static_cast<double>(p->kA[i]) * f_G;
 
 					// dres_i / dc_{p,i}
-					jac[i - bndIdx - offsetCp] = -qmax_times_ka * qSum_local * f_G;
+					jac[i - bndIdx - offsetCp] = -kA_times_fG * qFree_eff;
 					// Getting to c_{p,i}: -bndIdx takes us to q_0, another -offsetCp to c_{p,0} and a +i to c_{p,i}.
 					//                     This means jac[i - bndIdx - offsetCp] corresponds to c_{p,i}.
 
-					// dres_i / dpH, pH is the first c_{p,i}
-					jac[-bndIdx - offsetCp] = -(f_A_deriv * f_G + f_A * f_G_deriv - f_G_deriv * qsum) * qmax_times_ka * yCp[i];
+					// dres_i / d(salt axis), the first component is the selected salt representation
+					jac[-bndIdx - offsetCp] = -(qmax_times_ka * f_G * f_A_deriv * dqFreeEff_dqFree + static_cast<double>(p->kA[i]) * f_G_deriv * qFree_eff) * yCp[i] * dSaltAxis_dC0;
 
 					// Fill dres_i / dq_j
 					int bndIdx2 = 0;
@@ -219,7 +320,7 @@ namespace cadet
 							continue;
 
 						// dres_i / dq_j, part 1
-						jac[bndIdx2 - bndIdx] = qmax_times_ka * f_G * yCp[i] / static_cast<double>(p->qMax[j]);
+						jac[bndIdx2 - bndIdx] = kA_times_fG * yCp[i] * dqFreeEff_dqFree;
 						// Getting to q_j: -bndIdx takes us to q_0, another +bndIdx2 to q_j. This means jac[bndIdx2 - bndIdx] corresponds to q_j.
 
 						++bndIdx2;
