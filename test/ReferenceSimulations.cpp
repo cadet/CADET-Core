a47d0d0dd279a0b39ea3b3c5e03327c0c74e35fc
Jan Breuer <j.m.br@gmx.de>
2026-03-06 16:17:28 +0100

    fixup! adapt tolerances

--- test/ReferenceSimulations.cpp
+++ test/ReferenceSimulations.cpp
@@ -18,8 +18,8 @@ TEST_CASE("Reference test: FV KOREN", "[Column_1D],[FV_KOREN],[Simulation],[CI]"
 {
 	std::string modelFilePath = std::string("/data/config_axKOREN_eq_N128.json");
 	std::string refFilePath = std::string("/data/ref_axKOREN_eq_N128.h5");
-	const std::vector<double> absTol = { 5e-12 };
-	const std::vector<double> relTol = { 1e-6 };
+	const std::vector<double> absTol = { 5e-10 };
+	const std::vector<double> relTol = { 1e-4 };
 
 	cadet::test::column::FVParams disc(128);
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
@@ -29,8 +29,8 @@ TEST_CASE("Reference test: FV KOREN non-equidistant grid", "[Column_1D],[FV_KORE
 {
 	std::string modelFilePath = std::string("/data/config_axKOREN_noneq_N128.json");
 	std::string refFilePath = std::string("/data/ref_axKOREN_noneq_N128.h5");
-	const std::vector<double> absTol = { 5e-12 };
-	const std::vector<double> relTol = { 1e-6 };
+	const std::vector<double> absTol = { 5e-10 };
+	const std::vector<double> relTol = { 1e-4 };
 
 	cadet::test::column::FVParams disc(128);
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
@@ -40,8 +40,8 @@ TEST_CASE("Reference test: FV WENO2", "[Column_1D],[FV_WENO2],[Simulation],[CI]"
 {
 	std::string modelFilePath = std::string("/data/config_axWENO2_eq_N128.json");
 	std::string refFilePath = std::string("/data/ref_axWENO2_eq_N128.h5");
-	const std::vector<double> absTol = { 5e-12 };
-	const std::vector<double> relTol = { 1e-6 };
+	const std::vector<double> absTol = { 5e-10 };
+	const std::vector<double> relTol = { 1e-4 };
 
 	cadet::test::column::FVParams disc(128);
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
