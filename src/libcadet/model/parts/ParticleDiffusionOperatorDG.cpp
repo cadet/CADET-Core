a01833a65c19021d468de0d133e625aa34bb4036
Jan Breuer <j.m.br@gmx.de>
2026-04-01 17:46:41 +0200

    revert making par_disc_type optional

--- src/libcadet/model/parts/ParticleDiffusionOperatorDG.cpp
+++ src/libcadet/model/parts/ParticleDiffusionOperatorDG.cpp
@@ -89,14 +89,12 @@ namespace parts
 
 		// Read particle discretization mode and default to "EQUIDISTANT"
 		_parDiscMode = ParticleDiscretizationMode::Equidistant;
-		if (paramProvider.exists("PAR_DISC_TYPE"))
-		{
-			std::string pdt = paramProvider.getString("PAR_DISC_TYPE");
-			if (pdt == "EQUIVOLUME")
-				_parDiscMode = ParticleDiscretizationMode::Equivolume;
-			else if (pdt == "USER_DEFINED")
-				_parDiscMode = ParticleDiscretizationMode::UserDefined;
-		}
+		std::string pdt = paramProvider.getString("PAR_DISC_TYPE");
+
+		if (pdt == "EQUIVOLUME")
+			_parDiscMode = ParticleDiscretizationMode::Equivolume;
+		else if (pdt == "USER_DEFINED")
+			_parDiscMode = ParticleDiscretizationMode::UserDefined;
 
 		if (paramProvider.exists("PAR_DISC_VECTOR"))
 		{
