354fd2fb0c843386f8187dcf66db3a59b9d32b70
Jan Breuer <j.m.br@gmx.de>
2026-06-05 12:48:39 +0200

    add 2comp langmuir reference test and update the two 1comp RBF/MLP tests

--- test/ReferenceSimulations.cpp
+++ test/ReferenceSimulations.cpp
@@ -298,10 +298,10 @@ TEST_CASE("Reference test: GPR binding with MLP kernel single component", "[Colu
 {
 	std::string modelFilePath = std::string("/data/config_COL1D_GRM_GPR_Shallow_MLP_7.json");
 	std::string refFilePath = std::string("/data/ref_COL1D_GRM_GPR_Shallow_MLP_7.h5");
-	const std::vector<double> absTol = { 5e-4 };
+	const std::vector<double> absTol = { 1e-5 };
 	const std::vector<double> relTol = { 1e-5 };
 
-	cadet::test::column::FVParams disc(20, 5, 3);
+	cadet::test::column::FVParams disc(32, 8, 3);
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
 }
 
@@ -312,6 +312,18 @@ TEST_CASE("Reference test: GPR binding with RBF kernel single component", "[Colu
 	const std::vector<double> absTol = { 1e-5 };
 	const std::vector<double> relTol = { 1e-5 };
 
-	cadet::test::column::FVParams disc(20, 5, 3);
+	cadet::test::column::FVParams disc(32, 8, 3);
+	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
+}
+
+TEST_CASE("Reference test: GPR binding with MLP kernel for competitive Langmuir 2 component", "[Column_1D],[GPRBinding],[Simulation],[CI]")
+{
+	std::string modelFilePath = std::string("/data/config_Col1D_LRM_langmuirGPR_2comp_benchmark1.json");
+	std::string refFilePath = std::string("/data/ref_Col1D_LRM_langmuirGPR_2comp_benchmark1.h5");
+	const std::vector<double> absTol = { 1e-5 };
+	const std::vector<double> relTol = { 1e-5 };
+
+	cadet::test::column::FVParams disc(32);
+	disc.setBulkDiscParam("WENO_ORDER", static_cast<int>(2));
 	cadet::test::column::testReferenceBenchmark(modelFilePath, refFilePath, "001", absTol, relTol, disc, false);
 }
