8da892015e4ed560477b2019290caa3cec812e74
Jan Breuer <j.m.br@gmx.de>
2026-06-06 15:33:27 +0200

    update header licenses

--- src/libcadet/model/ColumnModel2D-InitialConditions.cpp
+++ src/libcadet/model/ColumnModel2D-InitialConditions.cpp
@@ -1,14 +1,14 @@
-// =============================================================================
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// =================================================================================
 //  CADET
 //
 //  Copyright © 2008-present: The CADET-Core Authors
 //            Please see the AUTHORS.md file.
 //
 //  All rights reserved. This program and the accompanying materials
-//  are made available under the terms of the GNU Public License v3.0 (or, at
-//  your option, any later version) which accompanies this distribution, and
-//  is available at http://www.gnu.org/licenses/gpl.html
-// =============================================================================
+//  are made available under the terms of the GNU Affero General Public
+//  License v3.0 (or, at your option, any later version).
+// =================================================================================
 
 #include "model/ColumnModel2D.hpp"
 #include "model/BindingModel.hpp"
